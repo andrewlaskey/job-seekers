feaa74f866475f367464de4c5dbef3264d75abad
Andrew Laskey <andrew.laskey@gmail.com>
2025-06-19 10:17:31 -0700

    feat: add bluesky link

--- components/layout/global-footer.tsx
+++ components/layout/global-footer.tsx
@@ -7,6 +7,7 @@ export default function GlobalFooter() {
     <footer className="w-full max-w-5xl flex items-start justify-between border-t mx-auto text-left text-xs gap-8 py-16 px-5">
       <div className="flex flex-col items-start gap-4">
         <Link href="/privacy">Privacy Policy & Terms</Link>
+        <p>Made with ❤️ by <Link href="https://bsky.app/profile/andrewlaskey.bsky.social" target="_blank">Andrew Laskey</Link></p>
         <div className="flex flex-col gap-2 max-w-80">
           <p>If you find this site helpful, please considering making a one-time or monthly donation on Ko-fi</p>
           <Link href="https://ko-fi.com/timbertales" target="_blank">
